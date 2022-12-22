47b615f3fb24ed81b1593e0734cffdc7474eb34a
ramachintaguntla-wb <111288247+ramachintaguntla-wb@users.noreply.github.com>
2022-12-22 15:29:33 +0000

    test

---
+++ src/feature/Home.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+
+import Card from "./Card";
+
+function Home() {
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+  useEffect(() => {
+    fetchList(page);
+  }, [page]);
+
+  const fetchList = (page) => {
+    if(page < totalPages){}
+    axios
+      .get(
+        encodeURI(
+          `https://www.omdbapi.com/?apikey=7b54a5cb&s=star wars&page=${page}`
+        )
+      )
+      .then((res) => {
+        setTotalPages(Math.ceil(res.totalResults/10))
+        setList([...list, ...res.data.Search])
+      });
+  };
+
+
+  return (
+    <div className="container">
+      <div className="App movies">
+        {list.map((movie) => (
+          <Card movie={movie} key={movie.imdbID} />
+        ))}
+      </div>
+      <button onClick={() => {setPage(page + 1)}} disabled={page > totalPages}> Next</button>
+    </div>
+  );
+}
+
+export default Home;
